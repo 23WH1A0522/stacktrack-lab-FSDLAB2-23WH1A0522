957cdc7222acbb832c875e7b444bbed9d16a669b
23WH1A0522 <23wh1a0522@bvrithyderabad.edu.in>
2026-04-10 20:32:05 +0530

    added api calls

--- src/api/client.js
+++ src/api/client.js
@@ -1,2 +1,21 @@
 // TODO: Set the base URL for your API
+BASE_URL = 'http://bvrithcloud.com/';
+
+async function handleResponse(response) {
+  if (!response.ok) {
+    throw new Error('Failed to fetch data');
+  }
+
+  return response.json();
+}
+
+export async function getAllTasks() {
+  const response = await fetch(BASE_URL);
+  return handleResponse(response);
+}
+
+export async function getTaskById(taskId) {
+  const response = await fetch(`${BASE_URL}/${taskId}`);
+  return handleResponse(response);
+}
 
