360113c81cd811784d6ca1bd193c60be0c5f7462
23WH1A0522 <23wh1a0522@bvrithyderabad.edu.in>
2026-04-10 20:38:01 +0530

    routing implementation

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import Home from './pages/Home';
+import QuestionPage from './pages/QuestionPage';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';  
+
 
 function App() {
   return (
     <div>
-      <Home />
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/questions" element={<QuestionPage />} />
+        </Routes>
+      </Router>
 
       {/* TODO: Replace this placeholder with Routes */}
     </div>
